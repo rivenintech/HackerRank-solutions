ae325d6c5bb6535dd73a512cd28572abf774448c
RiveN <61630074+RiveN000@users.noreply.github.com>
2022-02-05 01:24:56 +0100

    Added 8 python problems' solutions

---
+++ Python/write_a_function.py
@@ -0,0 +1,21 @@
+# Given a year, determine whether it is a leap year. If it is a leap year, return the Boolean True, otherwise return False.
+
+# In the Gregorian calendar, three conditions are used to identify leap years:
+# - The year can be evenly divided by 4, is a leap year, unless:
+# - The year can be evenly divided by 100, it is NOT a leap year, unless:
+# - The year is also evenly divisible by 400. Then it is a leap year.
+
+def is_leap(year):
+    leap = False
+    
+    if year % 4 == 0:
+        leap = True
+        if year % 100 == 0:
+            leap = False
+            if year % 400 == 0:
+                leap = True
+
+    return leap
+
+year = int(input())
+print(is_leap(year))
